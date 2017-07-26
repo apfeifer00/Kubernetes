36d5de09031cfd637548561cb15ff27ec628a44f
apfeifer00 <alexander.pf@gmx.de>
2017-07-26 13:38:51 +0200

    Update concourse-installation.sh

--- concourse-installation.sh
+++ concourse-installation.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 mkdir concourse
 cd ./concourse/
-wget https://raw.githubusercontent.com/apfeifer00/Kubernetes/master/concourse-kubernetes/concourse-worker-rc.yaml
-wget https://raw.githubusercontent.com/apfeifer00/Kubernetes/master/concourse-kubernetes/concourse-web-svc.yaml
-wget https://raw.githubusercontent.com/apfeifer00/Kubernetes/master/concourse-kubernetes/concourse-web-rc.yaml
-wget https://raw.githubusercontent.com/apfeifer00/Kubernetes/master/concourse-kubernetes/concourse-secrets.yaml
-wget https://raw.githubusercontent.com/apfeifer00/Kubernetes/master/concourse-kubernetes/concourse-postgresql-svc.yaml
-wget https://raw.githubusercontent.com/apfeifer00/Kubernetes/master/concourse-kubernetes/concourse-postgresql-secrets.yaml
-wget https://raw.githubusercontent.com/apfeifer00/Kubernetes/master/concourse-kubernetes/concourse-postgresql-rc.yaml
-
-
+#wget https://raw.githubusercontent.com/apfeifer00/Kubernetes/master/concourse-kubernetes/concourse-worker-rc.yaml
+#wget https://raw.githubusercontent.com/apfeifer00/Kubernetes/master/concourse-kubernetes/concourse-web-svc.yaml
+#wget https://raw.githubusercontent.com/apfeifer00/Kubernetes/master/concourse-kubernetes/concourse-web-rc.yaml
+#wget https://raw.githubusercontent.com/apfeifer00/Kubernetes/master/concourse-kubernetes/concourse-secrets.yaml
+#wget https://raw.githubusercontent.com/apfeifer00/Kubernetes/master/concourse-kubernetes/concourse-postgresql-svc.yaml
+#wget https://raw.githubusercontent.com/apfeifer00/Kubernetes/master/concourse-kubernetes/concourse-postgresql-secrets.yaml
+#wget https://raw.githubusercontent.com/apfeifer00/Kubernetes/master/concourse-kubernetes/concourse-postgresql-rc.yaml
+git clone https://gist.github.com/ea2b9e1dc23665867d60a1b340a85246.git
 
 
 kubectl create -f concourse-worker-rc.yaml
