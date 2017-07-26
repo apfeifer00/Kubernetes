1066b999bc34045b04a0670c5911fed732f32b01
apfeifer00 <alexander.pf@gmx.de>
2017-07-26 13:54:44 +0200

    Update concourse-installation.sh

--- concourse-installation.sh
+++ concourse-installation.sh
@@ -10,10 +10,10 @@ wget https://gist.githubusercontent.com/apfeifer00/ea2b9e1dc23665867d60a1b340a85
 wget https://gist.githubusercontent.com/apfeifer00/ea2b9e1dc23665867d60a1b340a85246/raw/94a2e60f23f2990fad79ffc481450c82a7b08a8d/concourse-web-svc.yaml
 
 
-kubectl create -f concourse-worker-rc.yaml
-kubectl create -f concourse-web-svc.yaml
-kubectl create -f concourse-web-rc.yaml
-kubectl create -f concourse-secrets.yaml
-kubectl create -f concourse-postgresql-svc.yaml
-kubectl create -f concourse-postgresql-secrets.yaml
-kubectl create -f concourse-postgresql-rc.yaml
+kubectl create -f concourse-worker-rc.yaml 
+kubectl create -f concourse-web-svc.yaml 
+kubectl create -f concourse-web-rc.yaml 
+kubectl create -f concourse-secrets.yaml 
+kubectl create -f concourse-postgresql-svc.yaml 
+kubectl create -f concourse-postgresql-secrets.yaml 
+kubectl create -f concourse-postgresql-rc.yaml 
