21d74948c93e9a965de9200e73bda8326ce83bd9
apfeifer00 <alexander.pf@gmx.de>
2017-07-26 08:07:16 +0200

    Update Kuberenetes-Master-Installation.sh

--- Kuberenetes-Master-Installation.sh
+++ Kuberenetes-Master-Installation.sh
@@ -7,18 +7,20 @@ apt-get update && apt-get install -y apt-transport-https
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 cat < /etc/apt/sources.list.d/kubernetes.list
   deb http://apt.kubernetes.io/ kubernetes-xenial main
-^C
+
 apt-get update
 apt-get install -y docker.io
 apt-get install -y kubelet kubeadm kubectl kubernetes-cni
 
 #The configuration of the master node
-kubeadm init --config file.yaml --token 5678
+kubeadm init 
+cat /search/authentication/token_file
+cut */token_file
 
 # start the Cluster
-sudo cp /etc/kubernetes/admin.conf $HOME/
-sudo chown $(id -u):$(id -g) $HOME/admin.conf
-export KUBECONFIG=$HOME/admin.conf
+#sudo cp /etc/kubernetes/admin.conf $HOME/
+#sudo chown $(id -u):$(id -g) $HOME/admin.conf
+#export KUBECONFIG=$HOME/admin.conf
 
 # Not ready bug fix 
 #sed -i "s|ExecStart=/usr/bin/kubelet.*$|ExecStart=/usr/bin/kubelet \$KUBELET_KUBECONFIG_ARGS \$KUBELET_SYSTEM_PODS_ARGS \$KUBELET_DNS_ARGS \$KUBELET_AUTHZ_ARGS \$KUBELET_CAD$|g" /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
@@ -28,5 +30,5 @@ export KUBECONFIG=$HOME/admin.conf
 #export KUBECONFIG=$HOME/admin.conf
 
 # Installation des Kubernetes Dashboards:
-kubectl create -f https://rawgit.com/kubernetes/dashboard/master/src/deploy/kubernetes-dashboard.yaml
+#kubectl create -f https://rawgit.com/kubernetes/dashboard/master/src/deploy/kubernetes-dashboard.yaml
 
