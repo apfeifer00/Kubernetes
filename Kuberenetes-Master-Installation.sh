0fa7060b7c2bf843970ef1ad0a203deb96c75a31
apfeifer00 <alexander.pf@gmx.de>
2017-07-26 08:16:22 +0200

    Update Kuberenetes-Master-Installation.sh

--- Kuberenetes-Master-Installation.sh
+++ Kuberenetes-Master-Installation.sh
@@ -1,16 +1,27 @@
 #!/bin/bash
 
 #Installation der Benötigten packages:
-wget https://raw.githubusercontent.com/apfeifer00/Kubernetes/master/file.yaml
-chmod +x file.yaml 
-apt-get update && apt-get install -y apt-transport-https
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
-cat < /etc/apt/sources.list.d/kubernetes.list
-  deb http://apt.kubernetes.io/ kubernetes-xenial main
+#wget https://raw.githubusercontent.com/apfeifer00/Kubernetes/master/file.yaml
+#chmod +x file.yaml 
+#apt-get update && apt-get install -y apt-transport-https
+#curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+#cat < /etc/apt/sources.list.d/kubernetes.list
+ # deb http://apt.kubernetes.io/ kubernetes-xenial main
 
 apt-get update
 apt-get install -y docker.io
-apt-get install -y kubelet kubeadm kubectl kubernetes-cni
+#apt-get install -y kubelet kubeadm kubectl kubernetes-cni
+
+
+apt-get update && apt-get install -y apt-transport-https
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
+deb http://apt.kubernetes.io/ kubernetes-xenial main
+EOF
+apt-get update
+apt-get install -y kubelet kubeadm
+
+
 
 #The configuration of the master node
 kubeadm init 
