3ff79337c4b7ab044b6ad681afa392186517a331
apfeifer00 <alexander.pf@gmx.de>
2017-07-26 13:48:13 +0200

    Update concourse-installation.sh

--- concourse-installation.sh
+++ concourse-installation.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 mkdir concourse
 cd ./concourse/
-#wget https://raw.githubusercontent.com/apfeifer00/Kubernetes/master/concourse-kubernetes/concourse-worker-rc.yaml
-#wget https://raw.githubusercontent.com/apfeifer00/Kubernetes/master/concourse-kubernetes/concourse-web-svc.yaml
-#wget https://raw.githubusercontent.com/apfeifer00/Kubernetes/master/concourse-kubernetes/concourse-web-rc.yaml
-#wget https://raw.githubusercontent.com/apfeifer00/Kubernetes/master/concourse-kubernetes/concourse-secrets.yaml
-#wget https://raw.githubusercontent.com/apfeifer00/Kubernetes/master/concourse-kubernetes/concourse-postgresql-svc.yaml
-#wget https://raw.githubusercontent.com/apfeifer00/Kubernetes/master/concourse-kubernetes/concourse-postgresql-secrets.yaml
-#wget https://raw.githubusercontent.com/apfeifer00/Kubernetes/master/concourse-kubernetes/concourse-postgresql-rc.yaml
-git clone https://gist.github.com/ea2b9e1dc23665867d60a1b340a85246.git
+wget https://gist.github.com/apfeifer00/ea2b9e1dc23665867d60a1b340a85246
+wget https://gist.githubusercontent.com/apfeifer00/ea2b9e1dc23665867d60a1b340a85246/raw/94a2e60f23f2990fad79ffc481450c82a7b08a8d/concourse-postgresql-secrets.yaml
+wget https://gist.githubusercontent.com/apfeifer00/ea2b9e1dc23665867d60a1b340a85246/raw/94a2e60f23f2990fad79ffc481450c82a7b08a8d/concourse-postgresql-svc.yaml
+wget https://gist.githubusercontent.com/apfeifer00/ea2b9e1dc23665867d60a1b340a85246/raw/94a2e60f23f2990fad79ffc481450c82a7b08a8d/concourse-secrets.yaml
+wget https://gist.githubusercontent.com/apfeifer00/ea2b9e1dc23665867d60a1b340a85246/raw/94a2e60f23f2990fad79ffc481450c82a7b08a8d/concourse-web-rc.yaml
+wget https://gist.githubusercontent.com/apfeifer00/ea2b9e1dc23665867d60a1b340a85246/raw/94a2e60f23f2990fad79ffc481450c82a7b08a8d/concourse-worker-rc.yaml
+wget https://gist.githubusercontent.com/apfeifer00/ea2b9e1dc23665867d60a1b340a85246/raw/94a2e60f23f2990fad79ffc481450c82a7b08a8d/concourse-web-svc.yaml
 
 
 kubectl create -f concourse-worker-rc.yaml
