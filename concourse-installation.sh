769ad532ef83fef1ab71e48356f6e830ce2f042c
apfeifer00 <alexander.pf@gmx.de>
2017-07-27 08:10:05 +0200

    Update concourse-installation.sh

--- concourse-installation.sh
+++ concourse-installation.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-mkdir concourse
-cd ./concourse/
+
 wget https://gist.githubusercontent.com/apfeifer00/ea2b9e1dc23665867d60a1b340a85246/raw/94a2e60f23f2990fad79ffc481450c82a7b08a8d/concourse-postgresql-rc.yaml
 wget https://gist.githubusercontent.com/apfeifer00/ea2b9e1dc23665867d60a1b340a85246/raw/94a2e60f23f2990fad79ffc481450c82a7b08a8d/concourse-postgresql-secrets.yaml
 wget https://gist.githubusercontent.com/apfeifer00/ea2b9e1dc23665867d60a1b340a85246/raw/94a2e60f23f2990fad79ffc481450c82a7b08a8d/concourse-postgresql-svc.yaml
@@ -8,7 +7,6 @@ wget https://gist.githubusercontent.com/apfeifer00/ea2b9e1dc23665867d60a1b340a85
 wget https://gist.githubusercontent.com/apfeifer00/ea2b9e1dc23665867d60a1b340a85246/raw/94a2e60f23f2990fad79ffc481450c82a7b08a8d/concourse-web-rc.yaml
 wget https://gist.githubusercontent.com/apfeifer00/ea2b9e1dc23665867d60a1b340a85246/raw/94a2e60f23f2990fad79ffc481450c82a7b08a8d/concourse-worker-rc.yaml
 wget https://gist.githubusercontent.com/apfeifer00/ea2b9e1dc23665867d60a1b340a85246/raw/94a2e60f23f2990fad79ffc481450c82a7b08a8d/concourse-web-svc.yaml
-
 
 kubectl create -f concourse-worker-rc.yaml 
 kubectl create -f concourse-web-svc.yaml 
