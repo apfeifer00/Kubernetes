b4481fd83891f95c1ab74e59ae47ca62ece1591b
apfeifer00 <alexander.pf@gmx.de>
2017-07-28 10:03:31 +0200

    Update Kubernetes-Minion-Installation.sh

--- Kubernetes-Minion-Installation.sh
+++ Kubernetes-Minion-Installation.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-#export TOKEN=920452.0ce99965d74737a4
-#export MASTER_IP_ADRESSE=192.168.8.7
-#export MASTER_PORT=6443
-#read -p "Bitte Token eingeben:" TOKEN
-#read -p "Bitte die Master IP-Adresse eingeben:" Master_IP_ADRESSE
 
 # Installation der Benötigten packages:
 apt-get update
@@ -17,7 +12,7 @@ apt-get update
 apt-get install -y kubelet kubeadm
 
 # Beitritt ins Cluster
-kubeadm join --token $TOKEN $MASTER_IP_ADRESSE:$MASTER_PORT --skip-preflight-checks
+kubeadm join --token $token $MASTER_IP_ADRESSE:6443 --skip-preflight-checks
 
 # Not ready bug fix 
 sed -i "s|ExecStart=/usr/bin/kubelet.*$|ExecStart=/usr/bin/kubelet \$KUBELET_KUBECONFIG_ARGS \$KUBELET_SYSTEM_PODS_ARGS \$KUBELET_DNS_ARGS \$KUBELET_AUTHZ_ARGS \$KUBELET_CAD$|g" /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
